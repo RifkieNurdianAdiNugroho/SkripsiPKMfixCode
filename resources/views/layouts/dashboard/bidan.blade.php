c7ba60b0c302d6a7f25e3da1c5ad54ebf4a1c543
Rifkie Nurdian AdiNugroho <70508070+RifkieNurdianAdiNugroho@users.noreply.github.com>
2023-04-19 00:08:58 +0700

    edit last fix dan bidan  laypouting commit

---
+++ resources/views/layouts/dashboard/bidan.blade.php
@@ -0,0 +1,35 @@
+								 <div class="menu-item">
+									<div class="menu-content pt-8 pb-0">
+										<span class="menu-section text-muted text-uppercase fs-8 ls-1">Data</span>
+									</div>
+								 </div>
+
+								  <div data-kt-menu-trigger="click" class="menu-item menu-accordion ">
+									<a  href="{{url('data/posyandu')}}">
+									<span class="menu-link">
+										<span class="menu-icon">
+											<!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
+											<span class="svg-icon svg-icon-2">
+												<i class="fa fa-medkit"></i>
+											</span>
+											<!--end::Svg Icon-->
+										</span>
+											<span class="menu-title">Posyandu</span>
+										</span>
+									 </a>
+								  </div>
+
+								   <div data-kt-menu-trigger="click" class="menu-item menu-accordion ">
+									<a  href="{{url('data/balita')}}">
+									<span class="menu-link">
+										<span class="menu-icon">
+											<!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
+											<span class="svg-icon svg-icon-2">
+												<i class="fa fa-medkit"></i>
+											</span>
+											<!--end::Svg Icon-->
+										</span>
+											<span class="menu-title">Balita</span>
+										</span>
+									 </a>
+								  </div>
