f504549b350590e12f4a667df6a4f9a49b1463c3
Rifkie Nurdian AdiNugroho <70508070+RifkieNurdianAdiNugroho@users.noreply.github.com>
2023-04-19 20:19:54 +0700

    controller home dan top layout fix push commit

--- resources/views/layouts/dashboard/top.blade.php
+++ resources/views/layouts/dashboard/top.blade.php
@@ -30,6 +30,7 @@
 
 		<!-- budle js -->
 		<script src="{{url('assets/js/scripts.bundle.js')}}"></script>
+	
 	</head>
 			<!--begin::Body-->
 	<body id="kt_body" class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed aside-enabled aside-fixed" style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
