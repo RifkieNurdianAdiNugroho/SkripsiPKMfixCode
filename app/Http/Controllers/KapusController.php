9595f1d379e09cb00050d62d48253bc4108b2861
Rifkie Nurdian AdiNugroho <70508070+RifkieNurdianAdiNugroho@users.noreply.github.com>
2023-04-21 12:02:34 +0700

    kapus controller push

---
+++ app/Http/Controllers/KapusController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use DB;
+class KapusController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $data = DB::table('kapus as kp')
+                ->join('users as us','us.id','=','kp.user_id')
+                ->select('kp.*','us.email')
+                ->get();
+        return view('dashboard.kapus.index',compact('data'));
+    }
+
+    public function create()
+    {
+        return view('dashboard.kapus.add');
+    }
+
+    public function store(Request $request)
+    {
+        $check = DB::table('users')->where('email',$request->email)->first();
+        if($check)
+        {
+            return redirect('user/kapus/store')
+                ->with('error','Gagal menambahakan data Kepala Puskesmas email '.$request->email.' sudah terpakai mohon gunakan email yang lain');
+        }
+
+        $createdAt = Carbon::now('Asia/Jakarta')->format('Y-m-d H:i:s');
+        $userId = DB::table('users')->insertGetId([
+            'role'=>'kapus',
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+            'created_at'=>$createdAt,
+        ]);
+
+        DB::table('kapus')->insert([
+            'user_id'=>$userId,
+            'nama'=>$request->nama,
+            'alamat'=>$request->alamat,
+            'jabatan_fungsional'=>$request->jabatan_fungsional,
+            'no_tlp'=>$request->no_tlp,
+            'created_at'=>$createdAt
+        ]);
+        return redirect('user/kapus')->with('success','Berhasil menambahakan data Kepala Puskesmas');
+    }
+
+    public function edit($id)
+    {
+       $data = DB::table('kapus as kp')
+                ->join('users as us','us.id','=','kp.user_id')
+                ->where('kp.id',$id)
+                ->select('kp.*','us.email')
+                ->first();
+        if(!$data)
+        {
+            return redirect('user/kapus')->with('error','Tidak dapat menemukan data Kepala Puskesmas');
+        }
+        return view('dashboard.kapus.edit',compact('data'));
+    }
+
+    public function update(Request $request,$id)
+    {
+       $check = DB::table('users')->where('email',$request->email)->first();
+       if($check)
+       {
+            if($check->id != $request->user_id)
+            {
+                return redirect('user/kapus/edit/'.$id)
+                ->with('error','Gagal mengubah data Kepala Puskesmas email '.$request->email.' sudah terpakai mohon gunakan email yang lain');
+            }
+       }
+
+       $createdAt = Carbon::now('Asia/Jakarta')->format('Y-m-d H:i:s');
+
+       if($request->password != null)
+       {
+            DB::table('users')->where('id',$request->user_id)->update([
+                'role'=>'kapus',
+                'email'=>$request->email,
+                'password'=>bcrypt($request->password),
+                'updated_at'=>$createdAt,
+            ]);
+       }else{
+            DB::table('users')->where('id',$request->user_id)->update([
+                'role'=>'kapus',
+                'email'=>$request->email,
+                'updated_at'=>$createdAt,
+            ]);
+       }
+        
+
+        DB::table('kapus')->where('id',$id)->update([
+            'nama'=>$request->nama,
+            'alamat'=>$request->alamat,
+            'jabatan_fungsional'=>$request->jabatan_fungsional,
+            'no_tlp'=>$request->no_tlp,
+            'updated_at'=>$createdAt
+        ]);
+        return redirect('user/kapus')->with('success','Berhasil mengubah data Kepala Puskesmas');
+    }
+
+    public function delete($id)
+    {
+        $data = DB::table('kapus')->where('id',$id)->first();
+        if($data)
+        {
+            DB::table('users')->where('id',$data->user_id)->delete();
+            DB::table('kapus')->where('id',$id)->delete();
+            return redirect('user/kapus')->with('success','Berhasil menghapus data Kepala Puskesmas');
+        }else{
+            return redirect('user/kapus')->with('error','Tidak dapat menemukan data Kepala Puskesmas');
+        }
+    }
+}
