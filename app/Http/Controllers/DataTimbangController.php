a3f154847a3e5356077b7731b36f6753092153e4
Rifkie Nurdian AdiNugroho <70508070+RifkieNurdianAdiNugroho@users.noreply.github.com>
2023-05-18 06:31:43 +0700

    update push data timbang controller(old version)

--- app/Http/Controllers/DataTimbangController.php
+++ app/Http/Controllers/DataTimbangController.php
@@ -17,34 +17,49 @@ class DataTimbangController extends Controller
     {
         $userId = Auth::user()->id;
         $bidan = DB::table('bidan')->where('user_id',$userId)->first();
-        $data = DB::table('posyandu_hasil as ph')
-                ->join('balita as bt','bt.id','=','ph.balita_id')
-                ->join('posyandu_jadwal as pj','pj.id','=','ph.jadwal_id')
+        $data = DB::table('posyandu_jadwal as pj')
+                ->join('bidan as bd','bd.id','=','pj.bidan_id')
+                ->join('posyandu as pd','pd.id','=','pj.posyandu_id')
                 ->where('pj.bidan_id',$bidan->id)
-                ->select('ph.*','bt.nama','pj.tanggal','pj.type as jadwal_type')
+                ->where('pj.jenis','posyandu')
+                ->select('bd.nama as bidan_name','pj.tanggal','pj.jenis as jadwal_type','pd.nama_pos as posyandu_name')
+                ->orderBy('pj.tanggal')
                 ->get();
-        return view('dashboard.timbang.index',compact('data'));
+        $now = Carbon::now('Asia/Jakarta')->format('Y-m-d');
+        return view('dashboard.timbang.index',compact('data','now'));
     }
 
-    public function create()
+    public function create($id)
     {
         $userId = Auth::user()->id;
         $bidan = DB::table('bidan')->where('user_id',$userId)->first();
-        $jadwal = DB::table('posyandu_jadwal')->where('bidan_id',$bidan->id)->get();
+        $jadwal = DB::table('posyandu_jadwal')->where('id',$id)->get();
         $balita = DB::table('balita')->get();
-        return view('dashboard.timbang.add',compact('jadwal','bidan','balita'));
+        $hasil = DB::table('posyandu_hasil as ph')
+                 ->join('balita as bt','bt.id','=','ph.balita_id')
+                 ->where('ph.jadwal_id',$id)
+                 ->select('bt.nama as balita_name','ph.*')
+                 ->get();
+        return view('dashboard.timbang.add',compact('jadwal','bidan','balita','hasil'));
     }
 
     public function store(Request $request)
     {
+        $userId = Auth::user()->id;
+        $bidan = DB::table('bidan')->where('user_id',$userId)->first();
         $createdAt = Carbon::now('Asia/Jakarta')->format('Y-m-d H:i:s');
-        DB::table('kader')->insert([
-            'nama'=>$request->nama,
-            'no_tlp'=>$request->no_tlp,
-            'alamat'=>$request->alamat,
+        DB::table('posyandu_hasil')->insert([
+            'bidan_id'=>$bidan->id,
+            'jadwal_id'=>$request->jadwal_id,
+            'balita_id'=>$request->balita_id,
+            'umur'=>$request->umur,
+            'tb'=>$request->tb,
+            'bb'=>$request->bb,
             'created_at'=>$createdAt
         ]);
-        return redirect('data/jadwal/timbang')->with('success','Berhasil menambahakan data Timbangan');
+
+
+        return redirect()->back()->with('success','Berhasil menambahakan data Timbangan');
     }
 
     public function edit($id)
