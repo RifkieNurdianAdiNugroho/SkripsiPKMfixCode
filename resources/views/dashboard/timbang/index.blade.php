d9eb4eca3a33df78dc7ef23be197b6d3006ddc13
Rifkie Nurdian AdiNugroho <70508070+RifkieNurdianAdiNugroho@users.noreply.github.com>
2023-06-06 10:49:36 +0700

    push update index timbang progress

--- resources/views/dashboard/timbang/index.blade.php
+++ resources/views/dashboard/timbang/index.blade.php
@@ -25,118 +25,136 @@
         <div id="kt_content_container" class="container-xxl">
             <div class="card">
                 <div class="card-header border-0 pt-6">
+                    <form action="{{url('data/jadwal/timbang')}}">
                     <div class="card-title">
                         <div class="d-flex align-items-center position-relative my-1">
-                            <span class="svg-icon svg-icon-1 position-absolute ms-6">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="black" />
-                                    <path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="black" />
-                                </svg>
-                            </span>
-                            <input type="text" data-kt-user-table-filter="search" class="form-control form-control-solid w-250px ps-14" placeholder="Search Pos" />
+                            <input type="text" name="balita" class="form-control form-control-solid " 
+                                   placeholder="Cari nama balita" value="{{$request->balita}}" />
+                            &nbsp;
+                            <input type="text" name="ortu" class="form-control form-control-solid " 
+                                   placeholder="Cari nama ortu" value="{{$request->ortu}}"/>
+                            &nbsp;
+                            @if($role != 'bidan')
+                            <select class="form-control form-control-solid" name="bidan_id">
+                                <option value="" selected disabled>
+                                    Pilih Bidan
+                                </option>
+                                @foreach($bidan as $bidanKey => $bidanIitem)
+                                <option value="{{$bidanIitem->id}}" 
+                                        {{$request->bidan_id == $bidanIitem->id ? 'selected':''}}>
+                                    {{$bidanIitem->nama}}
+                                </option>
+                                @endforeach
+                            </select>
+                            &nbsp;
+                            @endif
+                             <select class="form-control form-control-solid" name="pos_id">
+                                <option value="" selected disabled>
+                                    Pilih Pos
+                                </option>
+                                @foreach($posyandu as $posyanduKey => $posyanduItem)
+                                <option value="{{$posyanduItem->id}}" 
+                                        {{$request->pos_id == $posyanduItem->id ? 'selected':''}}>
+                                    {{$posyanduItem->nama_pos}}
+                                </option>
+                                @endforeach
+                            </select>
+                            &nbsp;
+                            <input type="month" name="start_month" value="{{$request->start_month}}" class="form-control form-control-solid" />
+                            &nbsp;
+                            <input type="month" name="end_month" value="{{$request->end_month}}" class="form-control form-control-solid" />
+                            &nbsp;
+                            <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>
+                        </div>
+                    </div>
+                    </form>
+                    <form action="{{url('data/jadwal/timbang/store')}}" method="POST">
+                    @csrf
+                    <div class="card-title">
+                        <div class="d-flex align-items-center position-relative my-1">
+                            <button class="btn btn-success" name="save" type="submit" onclick="return confirm('Apakah yakin untuk menyimpan data?')">
+                                <i class="fa fa-save"></i>
+                            </button>
+                            &nbsp;
+                            <button class="btn btn-warning" name="hitung" type="submit" onclick="return confirm('Apakah yakin untuk menghitung data?')">
+                                <i class="fa fa-calculator"></i>
+                            </button>
                         </div>
                     </div>
                 </div>
                 <div class="card-body py-4">
                     <div class="table-responsive">
+                        @if(count($data) > 0)
                         <table class="table align-middle table-row-dashed fs-6 gy-5">
                             <tr>
-                                <th colspan="2" rowspan="2">Nama Posyandu</th>
-                                <th colspan="2" rowspan="2">Nama Anak</th>
-                                <th colspan="2" rowspan="2">Nama Ortu</th>
-                                <th colspan="6" style="text-align: center;">Mei</th>
-                                <th colspan="6" style="text-align: center;">Juni</th>
-                                <th colspan="6" style="text-align: center;">Agustus</th>
+                                <th colspan="2" rowspan="2" style="padding-top: 6%">Nama Posyandu</th>
+                                <th colspan="2" rowspan="2" style="padding-top: 6%">Nama Anak</th>
+                                <th colspan="2" rowspan="2" style="padding-top: 6%">Nama Ortu</th>
+                                @if(isset($data['bulan']))
+                                @foreach($data['bulan'] as $bulanKey => $bulanItem)
+                                <th colspan="6" style="text-align: center;">{{$bulanItem}}</th>
+                                @endforeach
+                                @endif
+                                <th colspan="2" rowspan="2" style="padding-top: 6%">Status Gizi</th>
                             </tr>
-
+                            
                             <tr>
-                                <th colspan="2" style="text-align: center;">Umur</th>
-                                <th colspan="2" style="text-align: center;">TB</th>
-                                <th colspan="2" style="text-align: center;">BB</th>
-
-                                <th colspan="2" style="text-align: center;">Umur</th>
-                                <th colspan="2" style="text-align: center;">TB</th>
-                                <th colspan="2" style="text-align: center;">BB</th>
-
-                                <th colspan="2" style="text-align: center;">Umur</th>
-                                <th colspan="2" style="text-align: center;">TB</th>
-                                <th colspan="2" style="text-align: center;">BB</th>
+                                @if(isset($data['bulan']))
+                                    @foreach($data['bulan'] as $bulanKey => $bulanItem)
+                                    <th colspan="2" style="text-align: center;padding-bottom: 2%;">Umur</th>
+                                    <th colspan="2" style="text-align: center;padding-bottom: 2%;">TB</th>
+                                    <th colspan="2" style="text-align: center;padding-bottom: 2%;">BB</th>
+                                   
+                                    <input type="hidden" name="pos_id" 
+                                           value="{{$data['jadwal'][$bulanKey]['jadwal_id']}}">
+                                    <input type="hidden" name="bidan_id" 
+                                           value="{{$data['jadwal'][$bulanKey]['bidan_id']}}">
+                                    <input type="hidden" name="jadwal_id[]" 
+                                           value="{{$data['jadwal'][$bulanKey]['jadwal_id']}}">
+                                    @endforeach
+                                @endif
                             </tr>
+                                @if(isset($data['balita']))
+                                @foreach($data['balita'] as $balitaKey => $balitaItem)
+                                <tr>    
+                                    <td colspan="2">{{$balitaItem['pos']}}</td>
+                                    <td colspan="2">{{$balitaItem['nama']}}</td>
+                                    <td colspan="2">{{$balitaItem['ortu']}}</td>
+                                    <input type="hidden" name="balita_id[]" value="{{$balitaItem['balita_id']}}">
 
-                            <tr>
-                                <td colspan="2">Klobuk Wetan</td>
-                                <td colspan="2">Bani</td>
-                                <td colspan="2">Khairy</td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-
-
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan="2">Klobuk Wetan</td>
-                                <td colspan="2">Karmila</td>
-                                <td colspan="2">Khairy</td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                                <td colspan="2">
-                                    <input type="number" class="form-control form-control-solid">
-                                </td>
-                            </tr>
-
-
+                                     @foreach($data['bulan'] as $bulanKeyOne => $bulanItemOne)
+                                    <td colspan="2">
+                                        <input type="number" name="umur[{{$balitaItem['balita_id']}}][{{$data['jadwal'][$bulanKeyOne]['jadwal_id']}}]" readonly value="{{$data['hasil'][$data['jadwal'][$bulanKeyOne]['jadwal_id']][$balitaKey]['umur']}}" 
+                                        class="form-control form-control-solid umur">
+                                    </td>
+                                   
+                                    <td colspan="2">
+                                    <input type="number" name="tb[{{$balitaItem['balita_id']}}][{{$data['jadwal'][$bulanKeyOne]['jadwal_id']}}]" 
+ 
+                                    value="{{$data['hasil'][$data['jadwal'][$bulanKeyOne]['jadwal_id']][$balitaKey]['tb']}}"
+                                        class="form-control form-control-solid tb">
+                                    </td>
+                                    <td colspan="2">
+                                        <input type="number" name="bb[{{$balitaItem['balita_id']}}][{{$data['jadwal'][$bulanKeyOne]['jadwal_id']}}]" 
+                                        value="{{$data['hasil'][$data['jadwal'][$bulanKeyOne]['jadwal_id']][$balitaKey]['bb']}}"
+                                        class="form-control form-control-solid bb">
+                                    </td>
+                                    @endforeach
+                                    <td colspan="2">
+                                        {{$balitaItem['status_gizi']}}
+                                    </td>
+                                </tr>
+                                @endforeach
+                                @endif
                         </table>
+                        @else
+                        <p align="center">
+                            Data belum ada
+                        </p>
+                        @endif
                     </div>
                 </div>
+            </form>
             </div>
         </div>
     </div>
@@ -179,7 +197,7 @@
 
         // Search Datatable --- official docs reference: https://datatables.net/reference/api/search()
         var handleSearchDatatable = () => {
-            const filterSearch = document.querySelector('[data-kt-user-table-filter="search"]');
+            const filterSearch = document.querySelector('[]');
             filterSearch.addEventListener('keyup', function(e) {
                 datatable.search(e.target.value).draw();
             });
