2b6ff132f4b1f300231f4a1397bba3be32d82304
Rifkie Nurdian AdiNugroho <70508070+RifkieNurdianAdiNugroho@users.noreply.github.com>
2023-06-14 13:02:23 +0700

    update view balita selected by pos

--- resources/views/dashboard/balita/index.blade.php
+++ resources/views/dashboard/balita/index.blade.php
@@ -25,24 +25,62 @@
         </div>
         <div class="post d-flex flex-column-fluid" id="kt_post">
             <div id="kt_content_container" class="container-xxl">
-                <div class="card">
+                
                     <div class="card-header border-0 pt-6">
-                        <div class="card-title">
-                            <div class="d-flex align-items-center position-relative my-1">
-                                <span class="svg-icon svg-icon-1 position-absolute ms-6">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none">
-                                        <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2"
-                                            rx="1" transform="rotate(45 17.0365 15.1223)" fill="black" />
-                                        <path
-                                            d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
-                                            fill="black" />
-                                    </svg>
-                                </span>
-                                <input type="text" data-kt-user-table-filter="search"
-                                    class="form-control form-control-solid w-250px ps-14" placeholder="Search user" />
-                            </div>
+                    <form action="{{url('data/balita')}}">
+                    <div class="card-title">
+                        <div class="d-flex align-items-center position-relative my-1" align="right">
+                             <input type="text" name="balita" class="form-control form-control-solid " 
+                                   placeholder="Cari nama balita" value="{{$request->balita}}" />
+                            &nbsp;
+                            <input type="text" name="ortu" class="form-control form-control-solid " 
+                                   placeholder="Cari nama ortu" value="{{$request->ortu}}"/>
+                            &nbsp;
+                            @if(Auth::user()->role == 'bidan')
+                            <select class="form-control form-control-solid" name="bidan_id">
+                                @foreach($bidan as $bidanKey => $bidanIitem)
+                                <option value="{{$bidanIitem->id}}" 
+                                        {{$request->bidan_id == $bidanIitem->id ? 'selected':''}}>
+                                    {{$bidanIitem->nama}}
+                                </option>
+                                @endforeach
+                            </select>
+                            @else
+                            <select class="form-control form-control-solid" name="bidan_id">
+                                <option value="" selected disabled>
+                                    Pilih Bidan
+                                </option>
+                                @foreach($bidan as $bidanKey => $bidanIitem)
+                                <option value="{{$bidanIitem->id}}" 
+                                        {{$request->bidan_id == $bidanIitem->id ? 'selected':''}}>
+                                    {{$bidanIitem->nama}}
+                                </option>
+                                @endforeach
+                            </select>
+                            @endif
+                            &nbsp;
+                           
+                             <select class="form-control form-control-solid" name="pos_id">
+                                <option value="" selected disabled>
+                                    Pilih Pos
+                                </option>
+                                @foreach($posyandu as $posyanduKey => $posyanduItem)
+                                <option value="{{$posyanduItem->id}}" 
+                                        {{$request->pos_id == $posyanduItem->id ? 'selected':''}}>
+                                    {{$posyanduItem->nama_pos}}
+                                </option>
+                                @endforeach
+                            </select>
+                            &nbsp;
+                            <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>
                         </div>
+                    </div>
+                    </form>
+                    </div>
+                
+                    <div class="card-header border-0 pt-6">
+
+                        @if(Auth::user()->role == 'ahli_gizi' || Auth::user()->role == 'bidan')
                         <div class="card-toolbar">
                             <div class="d-flex justify-content-end" data-kt-user-table-toolbar="base">
                                 <a href="{{ url('/data/balita/create') }}" class="btn btn-primary">
@@ -59,6 +97,7 @@
                                 </a>
                             </div>
                         </div>
+                        @endif
                     </div>
                     <div class="card-body py-4">
                         <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_crm_table">
@@ -91,6 +130,7 @@
                                             <small> {{$item->alamat}}</small>
                                         </td>
                                         <td class="text-end">
+                                            @if(Auth::user()->role == 'ahli_gizi' || Auth::user()->role == 'bidan')
                                             <a href="#" class="btn btn-light btn-active-light-primary btn-sm"
                                                 data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
                                                 <!--begin::Svg Icon | path: icons/duotune/arrows/arr072.svg-->
@@ -117,6 +157,7 @@
                                                         data-kt-users-table-filter="delete_row">Delete</a>
                                                 </div>
                                             </div>
+                                            @endif
                                         </td>
                                     </tr>
                                 @endforeach
