8cdead72c87b17016c7ebd54aa33ddb97ba4574f
Rifkie Nurdian AdiNugroho <70508070+RifkieNurdianAdiNugroho@users.noreply.github.com>
2023-06-18 16:18:27 +0700

    add data posyandu export

---
+++ app/Exports/DataPosyanduExport.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Exports;
+
+// use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Session;
+class DataPosyanduExport extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder  implements FromView,WithCustomValueBinder,ShouldAutoSize
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function view(): View
+    {
+        $data = Session::get('dataPosyandu');
+        $bidanPos = Session::get('bidanPos');
+        return view('dashboard.posyandu.excel', [
+            'data' => $data,
+            'bidanPos'=>$bidanPos
+        ]);
+    }
+}
