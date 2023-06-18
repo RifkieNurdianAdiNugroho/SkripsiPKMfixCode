38d073a02959d3be4b88dd5641f25fddd0a275cb
Rifkie Nurdian AdiNugroho <70508070+RifkieNurdianAdiNugroho@users.noreply.github.com>
2023-06-18 08:06:53 +0700

    push update index vitamin

--- resources/views/dashboard/posyandu/Jadwal/vitamin/index.blade.php
+++ resources/views/dashboard/posyandu/Jadwal/vitamin/index.blade.php
@@ -25,24 +25,76 @@
         </div>
         <div class="post d-flex flex-column-fluid" id="kt_post">
             <div id="kt_content_container" class="container-xxl">
+                <div class="card-header border-0 pt-6">
+                        <form action="{{url('data/jadwal/vitamin')}}">
+                            <div class="card-title">
+                                <div class="d-flex align-items-center position-relative my-1" align="right">
+                                     <input type="month" name="tanggal" class="form-control form-control-solid " 
+                                           placeholder="Cari nama balita" value="{{$request->tanggal}}" />
+                                    &nbsp;
+                                    @if(Auth::user()->role == 'bidan')
+                                    <select class="form-control form-control-solid" name="bidan_id">
+                                        @foreach($bidan as $bidanKey => $bidanIitem)
+                                        <option value="{{$bidanIitem->id}}" 
+                                                {{$request->bidan_id == $bidanIitem->id ? 'selected':''}}>
+                                            {{$bidanIitem->nama}}
+                                        </option>
+                                        @endforeach
+                                    </select>
+                                    @else
+                                     <select class="form-control form-control-solid" 
+                                             name="bidan_id" id="bidan_id" onchange="getPosyandu(this.value)">
+                                        <option value="" selected disabled>
+                                            Pilih Bidan
+                                        </option>
+                                        @foreach($bidan as $bidanKey => $bidanItem)
+                                        <option value="{{$bidanItem->id}}" 
+                                                {{$request->bidan_id == $bidanItem->id ? 'selected':''}}>
+                                            {{$bidanItem->nama}}
+                                        </option>
+                                        @endforeach
+                                    </select>
+                                    @endif
+                                    &nbsp;
+                                    @if(Auth::user()->role == 'ahli_gizi' || Auth::user()->role == 'kapus')
+                                    <select class="form-control form-control-solid" name="pos_id" id="pos_id">
+                                     @if($request->bidan_id != null)
+                                        <option value="" selected disabled>
+                                            Pilih Pos
+                                        </option>
+                                        @foreach($posyandu as $posyanduKey => $posyanduItem)
+                                        <option value="{{$posyanduItem->id}}" 
+                                                {{$request->pos_id == $posyanduItem->id ? 'selected':''}}>
+                                            {{$posyanduItem->nama_pos}}
+                                        </option>
+                                        @endforeach
+                                        @else
+                                         <option value="" selected disabled>
+                                            <small>(Pos) Pilih Bidan Terlebih Dahulu</small>
+                                        </option>
+                                        @endif
+                                    </select>
+                                    @else
+                                     <select class="form-control form-control-solid" name="pos_id">
+                                        <option value="" selected disabled>
+                                            Pilih Pos
+                                        </option>
+                                        @foreach($posyandu as $posyanduKey => $posyanduItem)
+                                        <option value="{{$posyanduItem->id}}" 
+                                                {{$request->pos_id == $posyanduItem->id ? 'selected':''}}>
+                                            {{$posyanduItem->nama_pos}}
+                                        </option>
+                                        @endforeach
+                                    </select>
+                                    @endif
+                                    &nbsp;
+                                    <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 <div class="card">
                     <div class="card-header border-0 pt-6">
-                        <div class="card-title">
-                            <div class="d-flex align-items-center position-relative my-1">
-                                <span class="svg-icon svg-icon-1 position-absolute ms-6">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none">
-                                        <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2"
-                                            rx="1" transform="rotate(45 17.0365 15.1223)" fill="black" />
-                                        <path
-                                            d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
-                                            fill="black" />
-                                    </svg>
-                                </span>
-                                <input type="text" data-kt-user-table-filter="search"
-                                    class="form-control form-control-solid w-250px ps-14" placeholder="Search user" />
-                            </div>
-                        </div>
                         @if(Auth::user()->role == 'bidan')
                         <div class="card-toolbar">
                             <div class="d-flex justify-content-end" data-kt-user-table-toolbar="base">
@@ -203,5 +255,40 @@
         KTUtil.onDOMContentLoaded(function() {
             crmList.init();
         });
+
+        function getPosyandu(val) {
+        $.ajax({
+                type: 'get',
+                url: "{{url('/user/bidan/posyandu')}}"+"/"+ val,
+                dataType: 'json',
+                success: function (data) {
+                    var temp = [];
+                    $.each(data, function (key, value) {
+                        temp.push({
+                            v: value,
+                            k: key
+                        });
+                    });
+
+                    var x = document.getElementById("pos_id");
+                    $('#pos_id').empty();
+                    var opt_head = document.createElement('option');
+                    opt_head.text = 'Pilih Pos';
+                    opt_head.value = '0';
+                    opt_head.disabled = true;
+                    opt_head.selected = true;
+                    x.appendChild(opt_head);
+                    for (var i = 0; i < temp[0].v.length; i++) {
+                        var opt = document.createElement('option');
+                        opt.value = temp[0].v[i].id;
+                        opt.text = temp[0].v[i].nama_pos;
+                        x.appendChild(opt);
+                    }
+                },
+                error: function (data) {
+                    console.log('Error:', data);
+                }
+            });
+       }
     </script>
 @endsection
